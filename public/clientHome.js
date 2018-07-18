fde56f842c3d1a7ee260c85cb7f9d0461f66e498
Jake Lewis <jakeaustinlewis@gmail.com>
2018-07-18 15:07:17 -0500

    profile page and sign up pages are mobile friendly

--- public/clientHome.js
+++ public/clientHome.js
@@ -5,5 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
             $('#sidebar').toggleClass('active');
         });
 
+
     });
+    console.log('page responding');
+    // addEventListener
+    let sidebar = document.getElementById("sidebar");
+    if (window.screen.width < 768) {
+        console.log('screen width less than 768');
+
+        if (sidebar.classList.contains('active')) {
+            console.log('active turned off?');
+            sidebar.classList.remove('active');
+        } else {
+            console.log('active turned off?');
+        }
+    }
 });
