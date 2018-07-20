d4d377a7940c1f050a1b3400bc23e759850cddeb
Jake Lewis <jakeaustinlewis@gmail.com>
2018-07-20 12:24:37 -0500

    fixed bcrypt compare logic

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,8 @@ const bcrypt = require('bcrypt');
 
 
 router.get('/login', (req, res) => {
-    console.log(typeof(req.session.loginError));
-    console.log(req.session.loginError);
+    console.log('type of loginError:', typeof(req.session.loginError));
+    console.log('login Error:',req.session.loginError);
     res.render('login', {
         title: 'Login Page', error: req.session.loginError
     });
@@ -33,42 +33,47 @@ router.post('/login', (req, res, next) => {
 
             } else {
                 currentUser = user[0];
+                console.log('Current User is:', currentUser);
                 let hash = currentUser.hashed_password;
-
-                if (bcrypt.compare(req.body.password, hash)) {
-           
-                    // The users's password is valid.
-                    req.session.user = currentUser;
-                    // res.send(currentUser);
-                    if(currentUser.userType==='client') {
-                        //if userType is client, go to client home page
-                        res.redirect('/clientHome');
-                    } else {
-                        //if userType is phebotomist, go to phlebotomist home page
-                        //
-                        //when /driverHome page is build, change bellow to res.redirect('/diverHome');
-                        res.redirect('/clientHome');
-                    }
-                    
-                } else {
-                    console.log("You're information is incorrect!")
-                    res.redirect('/login');
-                }
+                console.log('Hash', hash);
+                console.log('Front end password', req.body.password);
+                return bcrypt.compare(req.body.password, hash);
             }
         })
+        .then(function (result) {
 
+            if (result == true) {
+                // The users's password is valid.
+                req.session.user = currentUser;
+                // res.send(currentUser);
+                if(currentUser.userType==='client') {
+                    //if userType is client, go to client home page
+                    res.redirect('/clientHome');
+                } else {
+                    //if userType is phebotomist, go to phlebotomist home page
+                    //
+                    //when /driverHome page is build, change bellow to res.redirect('/diverHome');
+                    res.redirect('/clientHome');
+                }
+            } else {
+                let passwordIsIncorrect = new Error('Password is incorrect! Please Try Again.');
+                // req.session.loginError = 'Password is incorrect! Please Try Again.';
+                return Promise.reject(passwordIsIncorrect);
+                // console.log("You're information is incorrect!")
+                // res.redirect('/login');
+            }
+        }) 
         .catch(function (error) {
-            console.error('Create user error:', error);
-            if (error.message === 'Username does not exist') {
+            console.error('Login user error:', error);
+            if (error.message === 'Username does not exist' || error.message === 'Password is incorrect! Please Try Again.') {
                 req.session.loginError = error.message;
-                console.log(req.session.loginError);
+                console.log('login error:', req.session.loginError);
                 res.redirect('/login');
 
             } else {
                 res.status(500).send({
                     error: error.message
                 });
-                
             }
             // res.redirect('/login');
         })
